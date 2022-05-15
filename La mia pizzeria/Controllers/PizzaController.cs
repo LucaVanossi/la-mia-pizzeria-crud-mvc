920b5331ee7159316c3de5ea2b199fe125eb0181
LucaVanossi <vanossi.luca13@gmail.com>
2022-05-15 14:36:17 +0200

    Esercizio ok

--- La mia pizzeria/Controllers/PizzaController.cs
+++ La mia pizzeria/Controllers/PizzaController.cs
@@ -18,7 +18,7 @@ namespace La_mia_pizzeria.Controllers
         [HttpGet]
         public IActionResult Details (int id)
         {
-            Pizza pizzaFound = null;
+            Pizza pizzaFound = GetPizzaById(id);
 
             foreach(Pizza pizza in PostData.GetPizze())
             {
@@ -62,6 +62,89 @@ namespace La_mia_pizzeria.Controllers
             PostData.GetPizze().Add(pizzaConId);
 
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            Pizza pizzaToEdit = GetPizzaById(id);
+
+            if(pizzaToEdit == null)
+            {
+                return NotFound();
+            } else
+            {
+                return View("Update", pizzaToEdit);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Update(int id, Pizza modello)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("FormPizza", modello);
+            }
+
+            Pizza pizzaOriginal = GetPizzaById(id);
+
+            if (pizzaOriginal != null)
+            {
+                pizzaOriginal.Name = modello.Name;
+                pizzaOriginal.Description = modello.Description;
+                pizzaOriginal.Image = modello.Image;
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            int PizzaIndexToRemove = -1;
+
+            List<Pizza> pizzaList = PostData.GetPizze();
+            
+            for(int i=0; i < pizzaList.Count; i++)
+            {
+                if (pizzaList[i].Id == id);
+                {
+                    PizzaIndexToRemove = i;
+                }
+            }
+
+            if(PizzaIndexToRemove != -1)
+            {
+                PostData.GetPizze().RemoveAt(PizzaIndexToRemove);
+
+                return RedirectToAction("Index");
+            } else
+            {
+                return NotFound();
+            }
+            
+        }
+        
+        
+        private Pizza GetPizzaById(int id)
+        {
+            Pizza pizzaFound = null;
+
+            foreach (Pizza pizza in PostData.GetPizze())
+            {
+                if (pizza.Id == id)
+                {
+                    pizzaFound = pizza;
+                    break;
+
+                }
+            }
+
+            return pizzaFound;
         }
     }
 }
