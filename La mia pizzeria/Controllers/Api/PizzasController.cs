2d8c6748451c4868f6148eef9a4ca06aa1519529
LucaVanossi <vanossi.luca13@gmail.com>
2022-05-20 17:00:20 +0200

    Web api ok

---
+++ La mia pizzeria/Controllers/Api/PizzasController.cs
@@ -0,0 +1,27 @@
+﻿using La_mia_pizzeria.Data;
+using La_mia_pizzeria.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace La_mia_pizzeria.Controllers.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Pizza> pizze = new List<Pizza>();
+
+            using (BlogContext db = new BlogContext())
+            {
+                pizze = db.Pizze.ToList<Pizza>();
+            }
+
+            return Ok(pizze);
+        }
+        
+        
+    }
+}
